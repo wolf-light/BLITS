ee4b20c47db5c41788fa98f3affd3ab44b29c11d
chiar015 <chiar015@d.umn.edu>
2024-01-26 12:04:28 -0600

    hopefully fixed?

--- Pi/trackingscript.py
+++ Pi/trackingscript.py
@@ -1,18 +1,21 @@
 import time
 import pyrebase
+from pyrebase import pyrebase
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from firebase_admin import firestore
 
 # Initialize Firebase Realtime Database connection
+
 try:
     cred = credentials.Certificate(r"./bulldog-rocketry-a06cc-firebase-adminsdk-7hs4e-4c786d56ca.json")
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://bulldog-rocketry-a06cc-default-rtdb.firebaseio.com/testdata'
     })
     print("Connected to Firebase Realtime Database")
-    db.child('STIStest').push("tracking script functioning")
+    doc_ref = db.reference('/')
+    doc_ref.child('STIStest').push("tracking script functioning")
 except Exception as e:
     print(f"Failed to connect to Firebase Realtime Database: {e}")
 
