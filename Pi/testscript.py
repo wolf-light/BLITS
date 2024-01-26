7108aa681fdff757a8a9ac43fd24d005a97540a1
chiar015 <chiar015@d.umn.edu>
2024-01-26 10:17:38 -0600

    commit before branch

--- Pi/testscript.py
+++ Pi/testscript.py
@@ -1,37 +1,34 @@
-import serial.tools.list_ports
-import serial
-import time
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
-from firebase_admin import firestore
 
-import http.client as httplib
+import time
+
+import threading
+
 
 from datetime import datetime
 
-def realtimeTest():
-    cred = credentials.Certificate(r"./realtimetest-11796-firebase-adminsdk-tbluh-04f6034e20.json")  # Replace with your service account JSON file path
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://realtimetest-11796-default-rtdb.firebaseio.com/'
-    })
+def print_numbers():
+    for i in range(5):
+        time.sleep(1)  # Simulating some work
+        print(f"Thread 1: {i}")
+
+def print_letters():
+    for char in 'ABCDE':
+        time.sleep(1)  # Simulating some work
+        print(f"Thread 2: {char}")
+
+# Create two threads
+thread1 = threading.Thread(target=print_numbers)
+thread2 = threading.Thread(target=print_letters)
+
+# Start the threads
+thread1.start()
+thread2.start()
+
+# Wait for both threads to finish
+thread1.join()
+thread2.join()
+
+print("Both threads have finished.")
     
-    ref = db.reference('/')
-    print("connected to firebase")
-    
-    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    for x in nums:
-        print(x)
-        ref.child('loop test').push(nums[x])
-        
-    
-    
-    print("\nDATA PUSHED TO REALTIME\n")
-    
-def main():
-    print("main function\n")
-    realtimeTest()
-    
-if __name__ == "__main__":
-    main()
+
     
