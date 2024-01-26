ec5cd9ddb9c8624a8cbb0199e70bddaf82437a97
Nathan Salberg <Salbe018@d.umn.edu>
2024-01-26 00:04:50 -0600

    Threaded the upload to firebase database

--- Pi/Serial_Read_Firebase.py
+++ Pi/Serial_Read_Firebase.py
@@ -4,9 +4,11 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import http.client as httplib
-
+from threading import Thread, RLock
+from typing import Dict
 # Alterable Variables
 COM_PORT = "COM4"
+LOCK = RLock()
 
 # Checks Internet Connection
 def internetConnectionPresent(url="www.google.com", timeout=3):
@@ -18,28 +20,56 @@ def internetConnectionPresent(url="www.google.com", timeout=3):
     except:
         return False
  
+class dbfuncs:
+    def __init__(self, max_points = 200):
+        self.max_points = 200
+        self.num_points_uploaded = 0
+        self.stopped = False
+        self.data = {}
+        self.internetConnection = internetConnectionPresent()
+        # Check for internet connection
+        # If true: Set up Firebase, clear current ThrustData
+        if (self.internetConnection):
+            cred = credentials.Certificate("firebase-key.json")
+            firebase_admin.initialize_app(cred)
+            self.db = firestore.client()
+            self.db.collection("DataTest").document("ourDoc").set({"ThrustData":{}}, merge = True)
+            print(f"Set internet connection to {self.internetConnection}")
 
-# Pushes data to Firebase Database    
-def pushtodb(db, dbcount, serial_read, obj1):
-    try:
-        if serial_read == "COLLECT" or serial_read == "SAFING":
-            return
-            
-        temp = serial_read.split(",")
-        temp[1] = float(temp[1]) * -1
-        temp[1] = str(temp[1])
-        obj1[temp[0]] = temp[1]
-        
-
-        if(dbcount < 200 and dbcount % 10  == 0):
-            if(internetConnectionPresent() == False):
-                internetConnection = False
+    def start(self):
+        t = Thread(target=self.upload, args=())
+        t.daemon = True
+        t.start()
+        return self
+    
+    def stop(self):
+        self.stopped = True
+    
+    def upload(self):
+        while True: 
+            if self.stopped:
                 return
-            db.collection("DataTest").document("ourDoc").set({"ThrustData":obj1}, merge = True) 
-            obj1 = {}
-    except:
-        internetConnection = False
+            with LOCK:
+                if (self.data and self.num_points_uploaded < 200 and len(self.data) > 3):
+                    self.pushtodb(self.data)
+                    self.data = {}
 
+    def pushtodb(self, data: Dict[str,str]) -> bool:
+        if(internetConnectionPresent() == False):
+            internetConnection = False
+            return
+        self.db.collection("DataTest").document("ourDoc").set({"ThrustData": data}, merge = True)
+
+
+def sendtothread(serial_read: str, dbfunctions: dbfuncs):
+    if serial_read == "COLLECT" or serial_read == "SAFING":
+        return
+    
+    temp = serial_read.split(",")
+    temp[1] = float(temp[1]) * -1
+    temp[1] = str(temp[1])
+    with LOCK:
+        dbfunctions.data[temp[0]] = temp[1]
 
 #================Main================
 def main():
@@ -57,39 +87,34 @@ def main():
         break
 
     print("Serial connected!")
-    print("Checking WiFi connection...")
-
-    # Check for internet connection
-    # If true: Set up Firebase, clear current ThrustData
-    if (internetConnectionPresent()):
-        cred = credentials.Certificate("firebase-key.json")
-        firebase_admin.initialize_app(cred)
-        db = firestore.client()
-        db.collection("DataTest").document("ourDoc").set({"ThrustData":{}}, merge = True)
-        internetConnection = True
-        print(f"Set internet connection to {internetConnection}")
-
+    print("initializing Database")
+    dbfunctions = dbfuncs().start()
     print("Waiting for collect command. Outputting raw info now")
 
     # Constantly read and output what it reads from serial port
     # If it receives a "COLLECT", start writing to a file
-    uploadCount = 0
-    global obj1 
-    obj1  = {}
     while True:
-        with open('data.txt', 'a') as file:
             if (ser.in_waiting > 0):
                 serial_read = ser.readline().strip().decode()
                 if(serial_read == "COLLECT") or collect == True:
                     collect = True
                     print(f"Writing: {serial_read}")
-                    file.write(serial_read)
-                    file.write("\n")
-                    if (internetConnection):
-                        pushtodb(db,dbcount, serial_read, obj1)
-                    dbcount += 1
+                    with open('data.txt', 'a') as file:
+                        file.write(serial_read + "\n")
+                    sendtothread(serial_read, dbfunctions)
                 else:
                     print(serial_read)
-            
+    
+def test_dbfuncs():
+    dbfunctions = dbfuncs().start()
+    with open('data_2.txt', 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        sendtothread(line, dbfunctions)
+        time.sleep(0.01)
+    time.sleep(3)
+    dbfunctions.stop()
+
 if __name__ == "__main__":
-    main()
+    test_dbfuncs()
+    #main()
